1ed4ab58c2b7f4e6c64ded045c273de059251b93
Ayush Shakya <ayshakya24@gmail.com>
2026-05-30 11:59:15 +0530

    Removed the cleanup worker to make it more serveless

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,15 @@ dotenv.config();
 
 import app from "./app.js";
 import { connectDB } from "./src/config/mongoDB.js";
-import { startReservationCleanupJob } from "./src/workers/reservationCleanup.job.js";
+import { reclaimExpiredStock } from "./src/services/reclaim.service.js";
 
 const PORT = process.env.PORT || 5000;
-let reservationJobTimer = null;
 
 const startServer = async () => {
   try {
     await connectDB();
-    reservationJobTimer = startReservationCleanupJob({
-      intervalMs: Number(process.env.RESERVATION_CLEANUP_INTERVAL_MS || 120000),
-      runOnBoot: true,
-    });
+
+    await reclaimExpiredStock();
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
@@ -26,11 +23,3 @@ const startServer = async () => {
 };
 
 startServer();
-
-process.on("SIGTERM", () => {
-  if (reservationJobTimer) clearInterval(reservationJobTimer);
-});
-
-process.on("SIGINT", () => {
-  if (reservationJobTimer) clearInterval(reservationJobTimer);
-});
